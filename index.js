ce34911021ad10ec66633efe85b5be767d3f65ac
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-06-27 09:59:17 -0700

    fix: relative path to local `schema.graphql` file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { readFileSync } = require("fs");
+const { join } = require("path");
 
 const semver = require("semver");
 const { graphql, buildSchema } = require("graphql");
@@ -8,7 +9,7 @@ const getEndpoint = require("./lib/get-endpoint");
 const getReleases = require("./lib/get-releases");
 const formatStringResolver = require("./lib/format-string-resolver");
 
-const schema = readFileSync("./schema.graphql", "utf8");
+const schema = readFileSync(join(__dirname, "schema.graphql"), "utf8");
 
 const resolvers = {
   Query: {
